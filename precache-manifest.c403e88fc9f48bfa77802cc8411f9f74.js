c48a9afa6b3da3d6d12833e8479da87fb569a836
Borys Lemeshko <lemeshkob@gmail.com>
2018-12-28 14:39:50 +0200

    Prod

---
+++ precache-manifest.c403e88fc9f48bfa77802cc8411f9f74.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "8edb0c51b940fb4fb3db",
+    "url": "./static/css/main.850e0060.chunk.css"
+  },
+  {
+    "revision": "8edb0c51b940fb4fb3db",
+    "url": "./static/js/main.8edb0c51.chunk.js"
+  },
+  {
+    "revision": "6f7429887ceef0ae32d3",
+    "url": "./static/js/1.6f742988.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "15954e12289d695ce4c6d299af65da15",
+    "url": "./static/media/logo.15954e12.png"
+  },
+  {
+    "revision": "516334735dbe3710e4a49db51ca5a5b7",
+    "url": "./static/media/cert.51633473.png"
+  },
+  {
+    "revision": "62974fff07b351c30926edc8df9daaeb",
+    "url": "./static/media/cert1.62974fff.png"
+  },
+  {
+    "revision": "1f8af803c1844807145c7f29f56e17a1",
+    "url": "./static/media/splash-right.1f8af803.png"
+  },
+  {
+    "revision": "099c377a9dc831e601f4803e97ba37ac",
+    "url": "./static/media/splash-left.099c377a.png"
+  },
+  {
+    "revision": "ff67a8258e384057a250de7044e83124",
+    "url": "./static/media/splash-left-not-main.ff67a825.png"
+  },
+  {
+    "revision": "5a14670a74e9982796a165118b67bfc8",
+    "url": "./static/media/ig-logo.5a14670a.png"
+  },
+  {
+    "revision": "5a6b584653cc3c692e024fd30bec43ae",
+    "url": "./static/media/leaves.5a6b5846.png"
+  },
+  {
+    "revision": "409f2eb5e5cb1a990b1d910428a47e2e",
+    "url": "./static/media/cardboard.409f2eb5.svg"
+  },
+  {
+    "revision": "5a96498f22b823a792b21ba3290e3580",
+    "url": "./static/media/cardboard-small.5a96498f.svg"
+  },
+  {
+    "revision": "d3215beb87f3b209aebe52fa9681777a",
+    "url": "./static/media/lustodorf.d3215beb.png"
+  },
+  {
+    "revision": "90c602e5f8cadfa129fb20bceee4f612",
+    "url": "./static/media/k-0.min.90c602e5.png"
+  },
+  {
+    "revision": "aa430ea0845ed36bf08d774594592b4e",
+    "url": "./static/media/k-1.min.aa430ea0.png"
+  },
+  {
+    "revision": "c66902faafa35abbce786b7193d02294",
+    "url": "./static/media/k-2.min.c66902fa.png"
+  },
+  {
+    "revision": "e7fee7396d9e30498dc547338f8962e4",
+    "url": "./static/media/k-3.min.e7fee739.png"
+  },
+  {
+    "revision": "d8aaea7fe023d3e30e613c0a11173d64",
+    "url": "./static/media/vegan.d8aaea7f.png"
+  },
+  {
+    "revision": "801608b1640712df4c96dba8ccf22079",
+    "url": "./static/media/vegan-official.801608b1.png"
+  },
+  {
+    "revision": "7b8b83f2275a9397f2d20c8c239c70f7",
+    "url": "./static/media/im-bg.7b8b83f2.png"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "./static/media/Proxima-Nova-Regular.410504d4.otf"
+  },
+  {
+    "revision": "6a386899746222073dd64c5f74d1a69d",
+    "url": "./static/media/Proxima-Nova-Semibold.6a386899.otf"
+  },
+  {
+    "revision": "62d4d7d369292a9bf23762465ec6d704",
+    "url": "./static/media/Proxima-Nova-Bold.62d4d7d3.otf"
+  },
+  {
+    "revision": "8a1b91a8e5ef27b6c7ac813ef2235dd8",
+    "url": "./static/media/LEngineer-Regular.8a1b91a8.otf"
+  },
+  {
+    "revision": "6af0db16607ef28c7039100f34216690",
+    "url": "./index.html"
+  }
+];
